958f301aaa6ba752125e1b4bf7f9ac1a62210aef
meharx07 <mehar.abdullahx07@gmail.com>
2026-07-08 17:57:31 +0500

    first file

---
+++ Basics/variables.js
@@ -0,0 +1,14 @@
+// variables are name give to a location in memory they are containers of data
+// in js the variables can be declared using let, const and var
+
+// constants are values that cannot be changed for eaxple a value of pie cannt be change
+const accountId = 12345
+let accountEmail = "mehar.abdullahx07@gmail.com"
+var accountPassword = 1234432
+accountCity = "Sargodha"
+
+// so as we know that js files are different from .txt and other js files have keywords and the compiler
+// node js or v8 knows those keywords and we cannout use js keywords as variable names and we dev use some methods to
+// define variables like using camelcase lie first word in small and secon wot with first letter capital like
+// accountPassword we can also use _ in between these are some habits but itss up to you if you follow them
+
