4245bf58bba289badfec229262fbfcda2d92ba01
meharx07 <mehar.abdullahx07@gmail.com>
2026-07-08 21:43:53 +0500

    changes

--- Basics/Datatypes.js
+++ Basics/Datatypes.js
@@ -1,27 +1,31 @@
 "use strict";
-// there is difference in js which was written in book 8 or 10 years ago and today js
-// in old js there were no arrow function modules and classes etc now js community didnt want to broke old
-// code due to new code so if you are using new code then we hav to tell that this is on new standard
-// so we write use strict no it will treat all code as newer version
 
-// in js we have two types of data
-// 1- primitive data types
-// 2- non-primitive data types
+// There is a massive difference between the JavaScript written 8 to 10 years ago and the modern JS we use today.
+// Old JS lacked arrow functions, modules, classes, and other modern features. 
+// The JavaScript community did not want to break old websites when introducing new features, so they made updates backward-compatible.
+// If you want your code to be treated under modern standards, you can explicitly tell the engine by writing "use strict"; at the top.
+// This forces the environment to execute all code using the stricter, newer rules.
 
-// here we will look at non primitive data types
+// In JavaScript, data types are divided into two main categories:
+// 1. Primitive data types (Storable directly in the stack memory)
+// 2. Non-Primitive data types (Reference types like Objects and Arrays)
 
-let name = "Mehar Abdullah" // string
-let age = 18 // integer
-let bigInteger = 1283749218374912384n // big int to store big integers use n at end o tell its big int
-let decimalInteger = 1.89 // here in js we dont have decimal the decimal are also integers
-let isLoggedIn = true // boolean
-let nothing = null // null is a stand alone value null is represantation of a empty value
-let notDefined = undefined // means we have declared a va but didnt give value to it
-let sym = Symbol("Id") // used to create completely unique identifiers.
+// Let's look closely at the Primitive Data Types:
 
-// to check the data types of a varibale you can use typeof
-// console.log(typeof name); // string
-// if we see type of null we get object
-console.log(typeof nothing); // object
+let name = "Mehar Abdullah";            // String: Used for text.
+let age = 18;                           // Number: Used for whole integers.
+let bigInteger = 1283749218374912384n;  // BigInt: Used for numbers larger than safe integer limits. Ends with an 'n'.
+let decimalInteger = 1.89;              // Number: In JS, decimals don't have a separate 'float' type; they are just 'numbers'.
+let isLoggedIn = true;                  // Boolean: Can only be true or false.
+let nothing = null;                     // Null: A standalone value representing an intentional, empty assignment.
+let notDefined = undefined;             // Undefined: The variable is declared, but has not been assigned a value yet.
+let sym = Symbol("Id");                 // Symbol: Used to create completely unique, unchangeable identifiers.
 
-// we will study every data type in detail later
+// To check the data type of a variable, you can use the 'typeof' operator:
+// console.log(typeof name);            // Output: "string"
+
+// Weird JS Quirk: If we check the type of null, it returns "object". 
+// This is a historic bug in JavaScript, but 'null' is conceptually a primitive value, not an object.
+console.log(typeof nothing);            // Output: "object"
+
+// We will study each data type in detail later.
