54a21623125adc74b887848fa7f0852fa4bf621d
meharx07 <mehar.abdullahx07@gmail.com>
2026-07-09 11:12:17 +0500

    new file

---
+++ Basics/Datatypes-Summary.js
@@ -0,0 +1,15 @@
+// in javascript datat ypes are divided in two categories
+// primitive types and non-primitive types
+// the difference in these two are how they are stored in memory and how to accecc them
+
+// there are 7 Primitive types
+let name = "Mehar Abdullah"                        // String
+let age = 18                                       // Number
+let isLoggedIn = false                             // boolean
+let city = undefined                               // undefined
+let temperature = null                             // null
+let bigNumber = 238719283719238127391823719283n    // bigInt
+let sym = Symbol('Id')                             // Symbol
+
+// primitive types are call by value means when we copy them from one place to another there reference of
+// memory is not share are given as copy and the things we change are only maded in copy
