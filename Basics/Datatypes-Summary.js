8c47f9b3691a7b902c8deb0023218c10c842b023
meharx07 <mehar.abdullahx07@gmail.com>
2026-07-09 15:36:21 +0500

    file

--- Basics/Datatypes-Summary.js
+++ Basics/Datatypes-Summary.js
@@ -13,3 +13,21 @@ let sym = Symbol('Id')                             // Symbol
 
 // primitive types are call by value means when we copy them from one place to another there reference of
 // memory is not share are given as copy and the things we change are only maded in copy
+
+// in non-primitive types we have
+// Arrays, Objects and functions
+
+// Arrays
+const heros = ["Batman", "Superman", "Aquaman"]
+
+// Objects
+const user = {
+    name: "Mehar Abdullah",
+    age: 18,
+    isLoggedIn: false
+}
+
+const greet = function(name){
+    console.log(`Hello ${name}`);
+}
+greet("Mehar Abdullah")
